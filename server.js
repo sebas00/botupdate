1b60e278884581752108b82a4a7fc2f3329623bd
Sebastiaan de Man <sebastiaan@gmail.com>
2019-02-17 19:51:44 +0100

    yes

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ expressWs(app, server);
 //const wss = new SocketServer({ server });
 app.ws('/', (ws, req) => {
   ws.send('hi');
+  ws.on('close', () => {return;})
   setInterval(() => {
+    
     ws.send(configobject.color);
   }, 1000);
 
@@ -31,8 +33,12 @@ ws.on('connection', (ws) => {
   ws.on('close', () => console.log('Client disconnected'));
 });})
 
-app.get('/what', (req, res) => {configobject.color = 'blue' ;
+app.get('/blue', (req, res) => {configobject.color = 'blue' ;
 res.json({color : 'blue'});})
+
+pp.get('/red', (req, res) => {configobject.color = 'red' ;
+res.json({color : 'red'});})
+
 
 app.use((req, res) => res.sendFile(INDEX) )
 server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
